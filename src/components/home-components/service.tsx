94295cd60e86f20b0d30df0ef8d5bf68c08a1c0a
Paulods0 <pauloluguenda0@gmail.com>
2024-06-20 15:44:17 +0100

    style:change services image & add change image while hover

--- src/components/home-components/service.tsx
+++ src/components/home-components/service.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+import { motion } from "framer-motion"
+import React, { useState } from "react"
 
 type Props = {
   service: {
@@ -6,15 +7,30 @@ type Props = {
     bg: string
     title: string
     img: string
+    imgHover: string
   }
 }
 
 const Service: React.FC<Props> = ({ service }) => {
-  const { img, title } = service
+  const { img, imgHover, title } = service
+  const [image, setImage] = useState(img)
+
+  function mouseEnter() {
+    setImage(imgHover)
+  }
+  function mouseLeave() {
+    setImage(img)
+  }
   return (
-    <div className="p-6 hover:bg-beeYellow-secondary transition-all ease-in-out duration-200 border border-beeBorder w-full lg:justify-start justify-center rounded-xl flex lg:flex-row flex-col items-center gap-4">
-      <img src={img} alt={title} className="object-contain h-14 w-20" />
-      <h1 className="text-xl text-center lg:text-left text-skin-text capitalize">{title}</h1>
+    <div
+      onMouseEnter={mouseEnter}
+      onMouseLeave={mouseLeave}
+      className="p-6 hover:bg-beeYellow-secondary transition-all ease-in-out duration-200 border border-beeBorder w-full lg:justify-start justify-center rounded-xl flex lg:flex-row flex-col items-center gap-4"
+    >
+      <img src={image} alt={title} className="object-contain h-14 w-20" />
+      <h1 className="text-xl text-center lg:text-left text-skin-text capitalize">
+        {title}
+      </h1>
     </div>
   )
 }
